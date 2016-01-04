df14be562d59521699db6f98ccf379e69e16b0fa
Gaurav Singh <sgaurav.baghel@gmail.com>
2016-01-04 13:21:43 +0530

    clean up extra files

--- steps/prepare-dirs/configure.sh
+++
@@ -1,3 +0,0 @@
-  cp -v ${MAGICX_CONF}/prepare-dirs /etc/init.d/prepare-dirs && \
-    chmod +x /etc/init.d/prepare-dirs && \
-    update-rc.d prepare-dirs defaults 01 99
--- steps/ssh/configure.sh
+++
@@ -1,4 +0,0 @@
-mkdir -pv /home/pi/.ssh && \
-  cat ${MAGICX_CONF}/id_radiodan.pub >> /home/pi/.ssh/authorized_keys && \
-  chown pi:pi /home/pi/.ssh/authorized_keys && \
-  chmod 600 /home/pi/.ssh/authorized_keys
