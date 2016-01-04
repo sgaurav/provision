c1e60dca22ec98a7d1b098440f3a541bb2aab836
Gaurav Singh <sgaurav.baghel@gmail.com>
2016-01-04 13:10:17 +0530

    added new steps in pre install code

--- steps/_post-install/install.sh
+++ steps/_post-install/install.sh
@@ -1,5 +1,5 @@
   chown -R pi:pi /opt/radiodan/
 
 # Write the creation time for future reference
-  echo "radiodan/provision (`date`)" > /boot/radiodan-provision-info && \
-  ln -sfv /boot/radiodan-provision-info /opt/radiodan/
+  # echo "radiodan/provision (`date`)" > /boot/radiodan-provision-info && \
+  # ln -sfv /boot/radiodan-provision-info /opt/radiodan/
--- steps/_pre-install/install.sh
+++ steps/_pre-install/install.sh
@@ -3,31 +3,41 @@
 # create .ssh dir to place authorised_keys later
   mkdir -pv /home/pi/.ssh
 
+#create dir where radiodan will save data
+  mkdir -pv /var/log/radiodan
+
 # remove default rubbish from /home/pi
   rm -rfv /home/pi/python_games && \
-    rm -rfv /home/pi/Desktop && \
-    rm -rfv /home/pi/ocr_pi.png
+    rm -rfv /home/pi/Pictures && \
+    rm -rfv /home/pi/Downloads && \
+    rm -rfv /home/pi/Videos && \
+    rm -rfv /home/pi/Music && \
+    rm -rfv /home/pi/Documents && \
+    rm -rfv /home/pi/Templates && \
 
 # remove rubbish from /opt/
-  rm -rf /opt/Wolfram
+  # rm -rf /opt/Wolfram
 
 # set jessie as main source of packages
-  cp /etc/apt/sources.list /etc/apt/sources.list.bak && \
-    sed -i -e 's/ \(stable\|wheezy\)/ testing/ig' /etc/apt/sources.list && \
-    cat /etc/apt/sources.list.bak >> /etc/apt/sources.list
+  # cp /etc/apt/sources.list /etc/apt/sources.list.bak && \
+  #   sed -i -e 's/ \(stable\|wheezy\)/ testing/ig' /etc/apt/sources.list && \
+  #   cat /etc/apt/sources.list.bak >> /etc/apt/sources.list
 
 # clean up distro
   apt-get update
 
 # remove unneeded stuff
-  apt-get purge -y lxde midori scratch weston && \
+  apt-get purge -y wolfram-engine libreoffice libreoffice-* claws-mail sonic-pi scratch bluej greenfoot nodered && \
     apt-get autoremove -y
 
+#install ruby
+  apt-get install ruby
+  
 # upgrade remaining packages
-  #apt-get upgrade -y
+  apt-get upgrade -y
 
 # update firmware
-  apt-get install -y rpi-update && rpi-update
+  # apt-get install -y rpi-update && rpi-update
 
 # Upstart
   #yes 'Yes, do as I say!' | apt-get -y --force-yes install upstart
